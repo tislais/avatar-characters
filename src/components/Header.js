73c85b235577a913534db345e5e517bd45e92ffe
Tis Lais <tistististis@gmail.com>
2021-08-03 20:01:14 -0700

    Add useEffect to container

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header 
-      className="bg-white fixed w-full
+      className="bg-white w-full
         h-16 flex items-center pl-4 z-10 border-b-4 border-gray-300">
       <h1 className="text-xl font-semibold text-gray-700 tracking-wide shadow-2xl">Avatar Characters</h1>
     </header>
--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CharacterList from '../components/CharacterList';
+import { fetchCharacters } from '../services/avatarApi';
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetchCharacters()
+      .then(res => setCharacters(res))
+      .then(() => setLoading(false));
+  }, []);
+
+  console.log(characters);
   if (loading) return <h1>Loading...</h1>;
-  return <CharacterList characters={characters} />;
+  // return <CharacterList characters={characters} />;
+  return <h1>Loaded.</h1>;
 };
 
 export default Characters;
