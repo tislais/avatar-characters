6d1c525ad98f95f5524bd8c4d6e77ce0d04ba051
Tis Lais <tistististis@gmail.com>
2021-08-04 10:42:26 -0700

    Adjust style

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -6,7 +6,6 @@ import { useParams } from 'react-router-dom';
 const CharacterDetail = ({ props }) => {
   const [character, setCharacter] = useState([]);
   const { id } = useParams();
-  console.log(props);
 
   useEffect(() => {
     fetchCharacterById(id)
@@ -15,7 +14,7 @@ const CharacterDetail = ({ props }) => {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="flex flex-col bg-white p-12 mt-12 rounded-xl shadow-md w-1/3 gap-y-4">
+      <div className="flex flex-col bg-white p-12 mt-12 rounded-xl shadow-md w-1/3 gap-y-2">
         <img src={character.photoUrl} alt={character.name} />
         <h1 className="text-3xl my-6">{character.name}</h1>
         <p><span className="font-bold">Gender: </span>{character.gender}</p>
