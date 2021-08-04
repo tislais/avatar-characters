0bc2776584818278b23efb2332b344fae8b6d2e4
Tis Lais <tistististis@gmail.com>
2021-08-04 10:39:39 -0700

    Add style

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -14,16 +14,18 @@ const CharacterDetail = ({ props }) => {
   }, [id]);
 
   return (
-    <>
-      <h1 className="text-4xl">{character.name}</h1>
-      <img src={character.photoUrl} alt={character.name} />
-      <p>Gender: {character.gender}</p>
-      <p>Hair Color: {character.hair}</p>
-      <p>Position: {character.position}</p>
-      <p>Affiliation: {character.affiliation}</p>
-      <p>Allies: {character.allies}</p>
-      <p>Enemies: {character.enemies}</p>
-    </>
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col bg-white p-12 mt-12 rounded-xl shadow-md w-1/3 gap-y-4">
+        <img src={character.photoUrl} alt={character.name} />
+        <h1 className="text-3xl my-6">{character.name}</h1>
+        <p><span className="font-bold">Gender: </span>{character.gender}</p>
+        <p><span className="font-bold">Hair Color: </span>{character.hair}</p>
+        <p><span className="font-bold">Position: </span>{character.position}</p>
+        <p><span className="font-bold">Affiliation: </span>{character.affiliation}</p>
+        <p><span className="font-bold">Allies: </span>{character.allies}</p>
+        <p><span className="font-bold">Enemies: </span>{character.enemies}</p>
+      </div>
+    </div>
   )
 };
 
